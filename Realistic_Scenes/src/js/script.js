b932bfbc9a8f9c5df2cb14dccbabd045a4d1598c
Atveek <atveekdungarani2021@gmail.com>
2024-12-16 12:12:50 +0530

    First Project is Complete

--- Realistic_Scenes/src/js/script.js
+++ Realistic_Scenes/src/js/script.js
@@ -1,7 +1,13 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
-const renderer = new THREE.WebGLRenderer();
+const hdrTextureUrl = new URL(
+  "../img/MR_INT-003_Kitchen_Pierre.hdr",
+  import.meta.url
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -13,9 +19,43 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(10, 10, 10);
+camera.position.set(0, 0, 10);
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
+
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1.8;
+
+const loader = new RGBELoader();
+loader.load(hdrTextureUrl, function (texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  //   scene.environment = texture;
+
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      color: 0xffea00,
+      roughness: 0,
+      metalness: 0.5,
+    })
+  );
+  scene.add(sphere);
+  sphere.position.set(2, 0, 0);
+
+  const sphere1 = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      color: 0x00ff00,
+      roughness: 0,
+      metalness: 0.5,
+      envMap: texture,
+    })
+  );
+  scene.add(sphere1);
+  sphere.position.set(-2, 0, 0);
+});
 
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
