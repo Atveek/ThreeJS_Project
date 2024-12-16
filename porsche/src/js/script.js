4e7773c2858b2f530cde8e8d78181003d47b3c5a
Atveek <atveekdungarani2021@gmail.com>
2024-12-16 12:40:32 +0530

    Second Project: Import Porsche from Sketchfab.com , Three Js setup is ready

---
+++ porsche/src/js/script.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(10, 10, 10);
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
